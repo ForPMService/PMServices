4d0689c09901d386c6a4467911f9470e33177b47
Aleksandr <buhfan@gmail.com>
2026-04-16 12:37:57 +0300

    Release issue 7.2, 7.4 v.1

--- src/IAM/PM.IAM.Contracts/obj/Debug/net10.0/PM.IAM.Contracts.AssemblyInfo.cs
+++ src/IAM/PM.IAM.Contracts/obj/Debug/net10.0/PM.IAM.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PM.IAM.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6868192426df3d97fd3c62081096e6bde3aa4c11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8323258f0ad99587d6afa7b179ae70df23306e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("PM.IAM.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PM.IAM.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
